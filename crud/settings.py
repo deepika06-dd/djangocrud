62a57c6b0e7e88b9d1be59042c66836bce6cec73
Deepika <laxmisipika98@gmail.com>
2026-03-30 22:00:47 +0545

    Update ALLOWED_HOSTS for Render

--- crud/settings.py
+++ crud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!idfz0r$t0cxzkx5bxqp8u5$7=fkp_quvjj=o@5%7h@8aov&^j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangocrud-1-sxdy.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
